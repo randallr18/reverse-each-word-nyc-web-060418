2713abad9efe3c8c5aa598938c5525d529588e28
Risher Randall <randallr18@mail.wlu.edu>
2018-05-31 21:45:15 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,11 @@
 def reverse_each_word(string)
   array = string.split(' ')
+  words_reversed = []
   
   array.each do |x|
+    words_reversed.unshift(x)
+  end
+  
+  words_reversed.join(' ')
+end
     
