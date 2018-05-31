5f926619f40133df8a31f9f3086bfe114f48c92b
Risher Randall <randallr18@mail.wlu.edu>
2018-05-31 22:07:32 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+# def reverse_each_word(string)
+#   array = string.split(' ')
+#   words_reversed = []
+  
+#   array.each do |x|
+#     words_reversed.push(x.reverse)
+#   end
+  
+#   words_reversed.join(' ')
+# end
+
+def reverse_each_word(string)
+    array = string.split(' ')
+    
+    words_reversed = []
+  array.collect do |x|
+    words_reversed.push(x.reverse)
+  end
+  
+  words_reversed.join(' ')
+end
